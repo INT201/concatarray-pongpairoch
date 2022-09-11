77207e98853da54f3431ac7180cbf81612e4249a
pongpairoch <pongpairoch.pongpairoch@mail.kmutt.ac.th>
2022-09-11 22:25:15 +0700

    2

--- concatArray.js
+++ concatArray.js
@@ -1,7 +1,23 @@
 const { template } = require('@babel/core')
 
 function concatArray(array1, array2) {
-  //code here
-  // test
+  if((array1 === null || array1 === undefined) && (array2 === null || array2 === undefined))
+  {
+    return undefined;
+  }
+  else if((array1?.length === 0) && (array2?.length === 0))
+  {
+    return array1;
+  }
+  else if((array1 === null || array1 === undefined || array1.length ===0) )
+  {
+    return array2;
+  }
+  else if((array2 === null || array2 === undefined || array2.length === 0))
+  {
+    return array1;
+  }
+  return array1.concat(array2);
+
 }
 module.exports = concatArray
